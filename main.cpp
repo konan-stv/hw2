8930e872df36796b338d8282f740ea5b70627679
konan-stv <konan@konanlabs.ru>
2019-01-17 19:20:51 +0300

    file format check

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <boost/range/adaptor/reversed.hpp>
 
 std::multiset <unsigned long> ip;
+int lnum = 1;
 
 auto sep(std::string &l) {
     l.erase(l.find('\t'));
@@ -15,14 +16,37 @@ auto sep(std::string &l) {
     return std::make_tuple(a[0], a[1], a[2], a[3]);
 }
 
+int ccount(std::string l, char c) {
+  if (l.find(c) == std::string::npos) return 0;
+  else return ccount(l.substr(l.find(c)+1), c) + 1;
+}
+
+int chk(std::string &l) {   // returns 1 in case of error
+    if (ccount(l, '\t') != 2) {
+        std::cout << "Line format error at line " << lnum << std::endl;
+        return 1;
+    }
+    if (ccount(l.substr(0, l.find('\t')), '.') != 3) {
+        std::cout << "IP address format error at line " << lnum << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
     std::string ln;
     
     while (std::getline(std::cin, ln)) {
+        if (chk(ln)) return 0;
         unsigned char a,b,c,d;
         std::tie(a, b, c, d) = sep(ln);
+        if (!d) {
+            std::cout << "Broadcast IP address forbidden at line " << lnum << std::endl;
+            return 0;
+        }
         //printf("%d %d %d %d\r\n", a, b, c, d);
         ip.insert((a<<24)+(b<<16)+(c<<8)+d);
+        lnum++;
     }
     
     for (auto const& val : boost::adaptors::reverse(ip)) {
