c1628d8120f98d17b360bb38ebc69b1cf5c045af
konan-stv <konan@konanlabs.ru>
2019-01-23 21:21:10 +0300

    minors

--- ip_filter.h
+++ ip_filter.h
@@ -12,7 +12,12 @@ auto sep(std::string &l) {
     l.erase(l.find('\t'));
     uint8_t a[4];
     int i = 0;
-    while (l.length()) [&a, &i](std::string &s) {a[i++]=stol(s.substr(0, s.find('.'))); s.erase(0, s.find('.')); s.erase(0, 1);}(l);
+    auto lf = [&a, &i](std::string &s) {
+      a[i++]=stol(s.substr(0, s.find('.'))); 
+      s.erase(0, s.find('.')); 
+      s.erase(0, 1);
+    };
+    while (l.length()) lf(l);
     return std::make_tuple(a[0], a[1], a[2], a[3]);
 }
 
@@ -21,7 +26,7 @@ int ccount(std::string l, char c) {
   else return ccount(l.substr(l.find(c)+1), c) + 1;
 }
 
-int chk(std::string &l) {   // returns 1 in case of error
+int chk(std::string &l) {   // returns 1 in case of syntax error
     if (ccount(l, '\t') != 2)
         return 1;
     if (ccount(l.substr(0, l.find('\t')), '.') != 3)
@@ -33,7 +38,7 @@ void printIP(uint32_t v) {
     std::cout << ((v>>24)&0xFF) << '.' << ((v>>16)&0xFF) << '.' << ((v>>8)&0xFF) << '.' << (v&0xFF) << std::endl;
 }
 
-uint8_t getOct(uint32_t v, uint8_t o1) {   // o is 1-based, left is 1st
+uint8_t getOct(uint32_t v, uint8_t o1) {   // o is 1-based, MSO (left) is 1st
     return (v>>((4-o1)*8))&0xFF;
 }
 
@@ -42,12 +47,9 @@ uint32_t packIP(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
 }
 
 int parseIP(std::string& l, ipset& i) {
-  //std::cout << l << std::endl;
-    uint8_t a,b,c,d;
-    std::tie(a, b, c, d) = sep(l);
+    auto [a, b, c, d] = sep(l);
     if (!d)
         return 1;
-    //printf("%d %d %d %d\r\n", a, b, c, d);
     i.insert(packIP(a, b, c, d));
     lnum++;
     return 0;
--- main.cpp
+++ main.cpp
@@ -3,40 +3,42 @@
 #include "ip_filter.h"
 
 ipset ip;
+uint8_t e = 0;
 
 int main() {
-    std::string ln;
-    
-    while (std::getline(std::cin, ln)) {
-        int a = chk(ln);
-        if (a == 1) {
-          std::cout << "Line format error at line " << lnum << std::endl;
-          return 1;
-        } else if (a == 2) {
-          std::cout << "IP address format error at line " << lnum << std::endl;
-          return 1;
-        }
-        if (parseIP(ln, ip)) {
-          std::cout << "Broadcast IP address forbidden at line " << lnum << std::endl;
-          return 1;
-        }
+  std::string ln;
+  
+  while (std::getline(std::cin, ln)) {
+    switch (chk(ln)) {
+      case 0: if (parseIP(ln, ip)) {
+        std::cout << "Broadcast IP address forbidden at line " << lnum << std::endl; 
+        e = 1; 
+      } break;
+      case 1: std::cout << "Line format error at line " << lnum << std::endl; e = 1; break;
+      case 2: std::cout << "IP address format error at line " << lnum << std::endl; e = 1; break;
     }
-    
-    for (auto const& val : boost::adaptors::reverse(ip)) {
-        printIP(val);
-    }
-    for (auto const& val : boost::adaptors::reverse(ip)) {
-        if (getOct(val,1) == 1)
-            printIP(val);
-    }
-    for (auto const& val : boost::adaptors::reverse(ip)) {
-        if ((getOct(val, 1) == 46) && (getOct(val, 2) == 70))
-            printIP(val);
-    }
-    for (auto const& val : boost::adaptors::reverse(ip)) {
-        if ((getOct(val, 1) == 46) || (getOct(val, 2) == 46) || (getOct(val, 3) == 46) || (getOct(val, 4) == 46))
-            printIP(val);
-    }
+  }
+  
+  if (e) {
+    std::cout << "Error(s) detected in the incoming data, aborting." << std::endl;
+    return 1;
+  }
+  
+  for (auto const& val : boost::adaptors::reverse(ip)) {
+    printIP(val);
+  }
+  for (auto const& val : boost::adaptors::reverse(ip)) {
+    if (getOct(val,1) == 1)
+      printIP(val);
+  }
+  for (auto const& val : boost::adaptors::reverse(ip)) {
+    if ((getOct(val, 1) == 46) && (getOct(val, 2) == 70))
+      printIP(val);
+  }
+  for (auto const& val : boost::adaptors::reverse(ip)) {
+    if ((getOct(val, 1) == 46) || (getOct(val, 2) == 46) || (getOct(val, 3) == 46) || (getOct(val, 4) == 46))
+      printIP(val);
+  }
 
-    return 0;
+  return 0;
 }
